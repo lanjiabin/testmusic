1b590aafe16abba523b6c774512aa48be49dd80a
lanjiabin <1323457707@qq.com>
2020-03-31 18:16:27 +0800

    4.个别activity添加toolbar

--- app/src/main/java/com/lanjiabin/testmusic/VideosPlayActivity.java
+++ app/src/main/java/com/lanjiabin/testmusic/VideosPlayActivity.java
@@ -3,6 +3,7 @@ package com.lanjiabin.testmusic;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -21,5 +22,19 @@ public class VideosPlayActivity extends Activity {
         MediaController mediaController=new MediaController(this);
         videoView.setMediaController(mediaController);
         videoView.requestFocus();
+        videoView.start();
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        super.dispatchKeyEvent(event);
+        if (event.getAction()!=KeyEvent.ACTION_DOWN){
+            if (event.getKeyCode()==KeyEvent.KEYCODE_BACK){
+                Intent intent=new Intent();
+                intent.setClass(this,AllVideosActivity.class);
+                startActivity(intent);
+            }
+        }
+        return true;
     }
 }
